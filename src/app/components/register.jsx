60194a672b5e088541c9bccbe3f25afe12bdaf44
mohmeed <mhmadkhlifah79@gmail.com>
2025-01-22 19:30:43 +0200

    edit table

--- src/app/components/register.jsx
+++ src/app/components/register.jsx
@@ -4,6 +4,7 @@ import ArrowUpRight from "../icon/arrow-up-right";
 import BarsArrowDown from "../icon/bars-arrow-down";
 import Delete from "../icon/delete";
 import DocumentArrowDown from "../icon/document-arrow-down";
+import ArrowPointingIn from "../icon/arrows-pointing-in";
 
 export default function Register() {
     const [shoDetails, setShowDetails] = useState(false)
@@ -15,20 +16,21 @@ export default function Register() {
         <div className=" ">
             <div className=" flex gap-5 w-full ">
                 <button className="px-12 py-2">بحث</button>
-                <input type="text" placeholder="from" />
-                <input type="text" placeholder="To" />
+                <input className="hover:bg-gray-100 focus:ring-0 " type="text" placeholder="from" />
+                <input className="hover:bg-gray-100 focus:ring-0 " type="text" placeholder="To" />
             </div>
                 {/* icon */}
             <div className=" flex gap-3 justify-end my-5">
                 <ArrowUpRight  />
                 <BarsArrowDown />
                 <DocumentArrowDown />
+                <ArrowPointingIn />
                 <Delete />
             </div>
 
             <div >
                 <table className=" w-full m-auto">
-                    <thead className="bg-gray-100">
+                    <thead className=" bg-gray-100">
                         <tr>
                             <th className=" p-2 border">النزع</th>
                             <th className=" p-2 border">الحالة</th>
@@ -138,51 +140,95 @@ export default function Register() {
                  <table className=" w-full text-sm text-center border-gray-300">
                      <thead className="bg-gray-100">
                          <tr>
+                             <th className=" p-2 border">رسالة الخطأ</th>
+                             <th className=" p-2 border">نوع الخطأ</th>
                              <th className=" p-2 border">الرمز</th>
                              <th className=" p-2 border">الوقت</th>
-                             <th className=" p-2 border">نوع الخطأ</th>
-                             <th className=" p-2 border">رسالة الخطأ</th>
+                             <th className=" p-2 border">الرسالة</th>
+                             <th className=" p-2 border">الحالة</th>
                          </tr>
                      </thead>
                      <tbody>
                          <tr>
+                             <td className=" p-2 border">Unknown WooCommerce Restful API.version</td>
+                             <td className=" p-2 border">معلومات</td>
                              <td className=" p-2 border">10337009</td>
-                             <td className=" p-2 border">12:31 PM</td>
-                             <td className=" p-2 border">حزف</td>
-                             <td className=" p-2 border">خطأ في المزامنة</td>
+                             <td className=" p-2 border">1:13:25 30/12/2024 PM</td>
+                             <td className="p-2 border"> عدد الموارد التي سوف يتم مزامنتها 343</td>
+                             <td className=" p-2 border">ناجح</td>
                          </tr>
                          <tr>
+                             <td className=" p-2 border">Unknown WooCommerce Restful API.version</td>
+                             <td className=" p-2 border">خطء</td>
                              <td className=" p-2 border">10337009</td>
-                             <td className=" p-2 border">12:31 PM</td>
-                             <td className=" p-2 border">حزف</td>
-                             <td className=" p-2 border">خطأ في المزامنة</td>
+                             <td className=" p-2 border">1:13:25 30/12/2024 PM</td>
+                             <td className="p-2 border"> حصل خطء خلال مزامنة المادة 1256800250</td>
+                             <td className=" p-2 border">فشل</td>
                          </tr>
                          <tr>
+                             <td className=" p-2 border">Unknown WooCommerce Restful API.version</td>
+                             <td className=" p-2 border">خطء</td>
                              <td className=" p-2 border">10337009</td>
-                             <td className=" p-2 border">12:31 PM</td>
-                             <td className=" p-2 border">حزف</td>
-                             <td className=" p-2 border">خطأ في المزامنة</td>
+                             <td className=" p-2 border">1:13:25 30/12/2024 PM</td>
+                             <td className="p-2 border"> حصل خطء خلال مزامنة المادة 1256800250</td>
+                             <td className=" p-2 border">فشل</td>
                          </tr>
                          <tr>
+                             <td className=" p-2 border">Unknown WooCommerce Restful API.version</td>
+                             <td className=" p-2 border">خطء</td>
                              <td className=" p-2 border">10337009</td>
-                             <td className=" p-2 border">12:31 PM</td>
-                             <td className=" p-2 border">حزف</td>
-                             <td className=" p-2 border">خطأ في المزامنة</td>
+                             <td className=" p-2 border">1:13:25 30/12/2024 PM</td>
+                             <td className="p-2 border"> حصل خطء خلال مزامنة المادة 1256800250</td>
+                             <td className=" p-2 border">فشل</td>
                          </tr>
                          <tr>
+                             <td className=" p-2 border">Unknown WooCommerce Restful API.version</td>
+                             <td className=" p-2 border">خطء</td>
                              <td className=" p-2 border">10337009</td>
-                             <td className=" p-2 border">12:31 PM</td>
-                             <td className=" p-2 border">حزف</td>
-                             <td className=" p-2 border">خطأ في المزامنة</td>
+                             <td className=" p-2 border">1:13:25 30/12/2024 PM</td>
+                             <td className="p-2 border"> حصل خطء خلال مزامنة المادة 1256800250</td>
+                             <td className=" p-2 border">فشل</td>
                          </tr>
                          <tr>
+                             <td className=" p-2 border">Unknown WooCommerce Restful API.version</td>
+                             <td className=" p-2 border">خطء</td>
                              <td className=" p-2 border">10337009</td>
-                             <td className=" p-2 border">12:31 PM</td>
-                             <td className=" p-2 border">حزف</td>
-                             <td className=" p-2 border">خطأ في المزامنة</td>
+                             <td className=" p-2 border">1:13:25 30/12/2024 PM</td>
+                             <td className="p-2 border"> حصل خطء خلال مزامنة المادة 1256800250</td>
+                             <td className=" p-2 border">فشل</td>
                          </tr>
                      </tbody>
                  </table>
+                 <div className=" flex justify-between mt-6">
+                 <div>
+                    <p>الظاهر 1 ال 10 من 344 سجل <select 
+                        id="options" 
+                        className=" p-2 border border-gray-300 rounded-md bg-white focus:ring-2 "
+                        >
+                        <option value="option1">1</option>
+                        <option value="option2">2</option>
+                        <option value="option3">3</option>
+                        <option value="option4">4</option>
+                        <option value="option5">5</option>
+                        <option value="option6">6</option>
+                        <option value="option7">7</option>
+                        <option value="option8">8</option>
+                        <option value="option9">9</option>
+                        <option value="option10"> 10</option>
+                        <option value="option11"> 11</option>
+                        <option value="option12"> 12</option>
+                    </select> لكل صفحه</p>
+                </div>
+                    <ul className=" li-register flex justify-end">
+                        <li>1</li>
+                        <li className=" bg-blue-600">2</li>
+                        <li>3</li>
+                        <li>4</li>
+                        <li>5</li>
+                        <li>...</li>
+                        <li>35</li>
+                    </ul>
+            </div>
              </div>
             )}
         </div>
--- src/app/icon/arrow-up-right.jsx
+++ src/app/icon/arrow-up-right.jsx
@@ -1,6 +1,6 @@
 export default function ArrowUpRight() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7 border border-black p-1 ">
           <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
         </svg>
     )
